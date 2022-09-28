4b8b339e73747c45cbb9a907b2f51ccfa1737878
Serovtsev Artem <1doncarleon@gmail.com>
2022-09-28 19:37:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketPrice = 20_000F;
+        int milePrice = 20;
+        int milesEarned = (int) ticketPrice / milePrice;
+        System.out.println(milesEarned);
+    }
+}
+
